89828c1efa3195c9c5de18c336c81de389c1523c
Anastasiia <anasta_ok@mail.ru>
2022-08-11 12:37:31 +0300

    added footer styles

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -14,50 +14,50 @@ const Footer = () => {
 
         <div className="col">
           <h5>Support</h5>
-          <span className="bar">
-            <a href="/">Contact up</a>
-            <a href="/">Chat</a>
-            <a href="/">Help Center</a>
-            <a href="/">FAQ</a>
-          </span>
+          <span className="bar"></span>
+          <a href="/">Contact up</a>
+          <a href="/">Chat</a>
+          <a href="/">Help Center</a>
+          <a href="/">FAQ</a>
         </div>
 
         <div className="col">
           <h5>Developers</h5>
-          <span className="bar">
-            <a href="/">Cloud</a>
-            <a href="/">Commerce</a>
-            <a href="/">Pro</a>
-            <a href="/">API</a>
-          </span>
+          <span className="bar"></span>
+          <a href="/">Cloud</a>
+          <a href="/">Commerce</a>
+          <a href="/">Pro</a>
+          <a href="/">API</a>
         </div>
 
         <div className="col">
           <h5>Company</h5>
-          <span className="bar">
-            <a href="/">About</a>
-            <a href="/">Information</a>
-            <a href="/">Legal</a>
-            <a href="/">Privacy</a>
-          </span>
+          <span className="bar"></span>
+          <a href="/">About</a>
+          <a href="/">Information</a>
+          <a href="/">Legal</a>
+          <a href="/">Privacy</a>
         </div>
 
         <div className="col">
           <h5>Social</h5>
-          <span className="bar">
-            <a href="/">
-              <FaFacebook className="icon" />
-            </a>
-            <a href="/">
-              <FaTwitter className="icon" />
-            </a>
-            <a href="/">
-              <FaLinkedin className="icon" />
-            </a>
-            <a href="/">
-              <FaGithub className="icon" />
-            </a>
-          </span>
+          <span className="bar"></span>
+          <a href="/">
+            <FaFacebook className="icon" />
+            Facebook
+          </a>
+          <a href="/">
+            <FaTwitter className="icon" />
+            Twitter
+          </a>
+          <a href="/">
+            <FaLinkedin className="icon" />
+            Linkedin
+          </a>
+          <a href="/">
+            <FaGithub className="icon" />
+            GitHub
+          </a>
         </div>
       </div>
     </div>
